e1638f54c0933cb2a058ac9db2b7200e2e00a65d
significantotter <significantotterwrites@gmail.com>
2022-09-17 01:31:46 -0400

    builds out character table more

--- migrations/20220917044714_character-tables.ts
+++ migrations/20220917044714_character-tables.ts
@@ -1,10 +1,23 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+	knex.schema.createTable('character_data', function (table) {
+		table.text('user_id');
+		table.integer('char_id');
+		table.unique(['user_id', 'char_id']);
+		table.jsonb('basic_data');
+		table.jsonb('metadata');
+		table.jsonb('calculated_stats');
+		table.jsonb('inventory');
+		table.jsonb('conditions');
+		table.integer('local_hp');
+		table.integer('local_experience');
+		table.integer('local_hero_points');
+		table.integer('local_focus_points');
+		table.jsonb('local_conditions');
+		table.jsonb('local_spellbook');
+		table.jsonb('local_spellslots');
+	});
 }
 
-
-export async function down(knex: Knex): Promise<void> {
-}
-
+export async function down(knex: Knex): Promise<void> {}
