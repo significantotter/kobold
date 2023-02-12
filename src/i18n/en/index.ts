d0988226a5e8a9b7e317f55c48a96cbe729bfb42
significantotter <significantotterwrites@gmail.com>
2023-02-12 00:22:18 -0500

    improves help command

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -219,10 +219,22 @@ const en: BaseTranslation = {
 							'the `attack` tag and every skill roll has the `skill` tag. A full list of tags are available under ' +
 							'`/help attributes-and-tags`. \n\nModifiers can be toggled active or inactive. When inactive, a ' +
 							'modifier will never apply to a roll, even if it applies to the given tags.\n\n\n' +
-							'Basic target-tags use the terms `and` and `or`. `and` means you needs BOTH tags in the roll. ' +
+							'**How to target rolls with target-tags**\n\n' +
+							'\t**or**\n\n' +
 							'`or` means you need EITHER tag in the roll.\n\n' +
-							"`attack and save` means the roll must be an attack roll AND a save roll, which doesn't happen!!\n\n" +
 							'`attack or save` means that the roll can either be an attack roll OR a save roll\n\n\n' +
+							'\t**and**\n\n' +
+							'`and` means you needs BOTH tags in the roll.\n\n' +
+							"`attack and save` means the roll must be an attack roll AND a save roll, which doesn't happen!!\n\n" +
+							'`skill and intimidation` would be true on any intimidation roll, because intimidation also has the skill tag!\n\n' +
+							'\t**not**\n\n' +
+							'`not` means that the roll applies to things that are not that tag \n\n' +
+							'`skill and not strength` applies to skill rolls that are NOT strength skills\n\n\n' +
+							'\t**Parentheses**\n\n' +
+							'Parentheses group tags. () \n\n' +
+							'`attack and (skill or dexterity)` requires attack and for the group to be valid! ' +
+							'So BOTH attack and EITHER skill or dexterity must be in the roll\n\n\n' +
+							'**Advanced**\n\n' +
 							'To learn how to build target tags ' +
 							'you can use [this link](https://github.com/joewalnes/filtrex), although its fairly technical. Alternatively, ' +
 							'here are some more examples to show how they can work:\n\n' +
