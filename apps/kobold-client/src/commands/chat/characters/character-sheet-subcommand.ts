046d789251512efd7384bbd179fee11358ca013e
SignificantOtter <113399196+significantotter@users.noreply.github.com>
2024-03-19 23:33:18 -0400

    Mar 19 bug fixes (#16)
    * fixes issue with /game init not working for all game character
    
    * adds sheet style option for /character sheet too

--- apps/kobold-client/src/commands/chat/characters/character-sheet-subcommand.ts
+++ apps/kobold-client/src/commands/chat/characters/character-sheet-subcommand.ts
@@ -7,11 +7,12 @@ import {
 
 import L from '../../../i18n/i18n-node.js';
 import { TranslationFunctions } from '../../../i18n/i18n-types.js';
-import { Kobold } from 'kobold-db';
+import { Kobold, SheetRecordTrackerModeEnum } from 'kobold-db';
 import { Creature } from '../../../utils/creature.js';
 import { InteractionUtils } from '../../../utils/index.js';
 import { KoboldUtils } from '../../../utils/kobold-service-utils/kobold-utils.js';
 import { Command, CommandDeferType } from '../../index.js';
+import { GameOptions } from '../game/game-command-options.js';
 
 export class CharacterSheetSubCommand implements Command {
 	public names = [L.en.commands.character.sheet.name()];
@@ -31,12 +32,15 @@ export class CharacterSheetSubCommand implements Command {
 		{ kobold }: { kobold: Kobold }
 	): Promise<void> {
 		const koboldUtils = new KoboldUtils(kobold);
+		const sheetStyle =
+			intr.options.getString(GameOptions.GAME_SHEET_STYLE.name) ??
+			SheetRecordTrackerModeEnum.full_sheet;
 		const { activeCharacter } = await koboldUtils.fetchNonNullableDataForCommand(intr, {
 			activeCharacter: true,
 		});
 
 		const creature = Creature.fromSheetRecord(activeCharacter.sheetRecord);
-		const embed = creature.compileEmbed('Sheet');
+		const embed = creature.compileEmbed('Sheet', sheetStyle);
 
 		await InteractionUtils.send(intr, embed);
 	}
