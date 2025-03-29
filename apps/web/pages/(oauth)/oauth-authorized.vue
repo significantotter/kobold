bfaa153c047a92bbc9558350f3e7fe45d5f8f6d2
significantotter <significantotterwrites@gmail.com>
2025-03-29 15:24:24 -0400

    adds peridot to the oauth authorized page

--- apps/web/pages/(oauth)/oauth-authorized.vue
+++ apps/web/pages/(oauth)/oauth-authorized.vue
@@ -2,7 +2,10 @@
 	<main
 		class="container mx-auto grid gap-auto grid-cols-1 child-margin-auto prose dark:prose-invert"
 	>
-		<h4>Yip! Success!</h4>
-		<h6>You may close the tab and return to discord.</h6>
+		<PeridotImage />
+		<div class="child-margin-auto">
+			<h1>Yip! Success!</h1>
+			<h3>You may close the tab and return to discord.</h3>
+		</div>
 	</main>
 </template>
