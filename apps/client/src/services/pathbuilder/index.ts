9ce241ba50751e3acd4711ebc01e263315684497
significantotter <significantotterwrites@gmail.com>
2026-03-01 16:23:56 -0500

    updates kobold's UA string to help with cloudflare issue for pathbuilder

--- apps/client/src/services/pathbuilder/index.ts
+++ apps/client/src/services/pathbuilder/index.ts
@@ -5,7 +5,13 @@ export class PathBuilder {
 	constructor() {}
 
 	public async get({ characterJsonId }: { characterJsonId: number }) {
-		return (await axios.get('https://pathbuilder2e.com/json.php?id=' + characterJsonId))
-			.data as PathBuilderTypes.JsonExport;
+		return (
+			await axios.get('https://pathbuilder2e.com/json.php?id=' + characterJsonId, {
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (compatible; KoboldBot/1.0; +https://github.com/significantotter/kobold)',
+				},
+			})
+		).data as PathBuilderTypes.JsonExport;
 	}
 }
